f90f8f8b8b1248761e729cca3732483ad356f893
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-09 23:41:04 +0200

    tests

--- my_strcpy.c
+++ my_strcpy.c
@@ -16,3 +16,21 @@ char *my_strcpy(char *dest, char const *src)
     dest[i] = '\0';
     return (dest);
 }
+
+/*
+** EPITECH PROJECT, 2018
+** my_strcpy.c
+** File description:
+** Task02
+*/
+
+char *my_strcpy(char *dest, char const *src, int n)
+{
+    int i = 0;
+
+    while (i < n) {
+	dest[i] = src[i];
+	i++;
+    }
+    return (dest);
+}
